15b19a12e2fbbd3627c2179cc09a389ac8ce21a2
Simon Walenkamp <simonwalenkamp@hotmail.com>
2024-04-09 22:31:59 +0200

    scaffold product search logic

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,5 +1,25 @@
+import { ProductSearchBuilder, Searcher, UserFactory } from "@relewise/client";
+import { useState } from "react";
 
 export default function Search() {
+  const [term, setTerm] = useState();
+
+  async function searchForProduct() {
+    const searcher = new Searcher("", "", { serverUrl: "" });
+
+    const request = new ProductSearchBuilder(
+      {
+        currency: "",
+        displayedAtLocation: "",
+        language: "",
+        user: UserFactory.anonymous()
+      }).build();
+
+    const result = await searcher.searchProducts(request);
+
+    console.log(result);
+  }
+
   return (
     <h1>Hello from search page!</h1>
   );
